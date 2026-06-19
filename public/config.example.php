1b29e8fe79351c50a70cc63a56f9ebbef32546d9
MLA62 <230602459+MLA62@users.noreply.github.com>
2026-06-19 21:53:29 +0200

    Parametrize profile text keys via DB

--- public/config.example.php
+++ public/config.example.php
@@ -3,7 +3,7 @@
 return [
     'app_name' => 'JeMa Jobs',
     'app_url' => 'https://jobs.jema.business',
-    'app_version' => '1.15.17',
+    'app_version' => '1.15.18',
     'app_key' => 'replace-with-64-random-hex-characters',
     'admin_emails' => ['admin@jema.business'],
     'db_host' => 'localhost',
